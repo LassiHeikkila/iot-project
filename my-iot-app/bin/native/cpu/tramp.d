ba8169735751d1eccd04756e87576dd61e68ca8e
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2023-12-16 19:08:17 +0200

    Delete my-iot-app/bin, add .gitignore

--- my-iot-app/bin/native/cpu/tramp.d
+++
@@ -1,5 +0,0 @@
-/data/riotbuild/riotproject/bin/native/cpu/tramp.o: \
- /data/riotbuild/riotbase/cpu/native/tramp.S /usr/include/stdc-predef.h \
- /data/riotbuild/riotproject/bin/native/riotbuild/riotbuild.h
-/usr/include/stdc-predef.h:
-/data/riotbuild/riotproject/bin/native/riotbuild/riotbuild.h:
